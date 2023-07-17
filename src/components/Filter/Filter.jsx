d986a818109c2f4b927e7f29de99e9c52b0c3c3a
AnnaAFA <anna.fedoruk111@gmail.com>
2023-07-17 16:11:35 +0300

    add contact book

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './Filter.module.css'
+
+export const Filter = ({filter, onFilterChange }) => {
+	return (
+		<>
+			<form className={css.filterForm}>
+				<label>
+					<span>Find contact by name</span>
+					<input
+						type="text"
+						value={filter}
+						onChange={onFilterChange}
+						className={css.filterInput}
+					/>
+				</label>
+			</form>
+		</>
+	)
+}
+
+Filter.propTypes = {
+	onFilterChange: PropTypes.func.isRequired,
+	filter: PropTypes.string.isRequired,
+
+};
